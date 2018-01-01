a325bb585e5dd829cb03437ca71150498ca7a40f
fangpapa <qpsa731@gmail.com>
2018-01-01 10:46:10 +0800

    Create SouthAsiaSignRule.java

---
+++ src/pattern/signrule/SouthAsiaSignRule.java
@@ -0,0 +1,14 @@
+package pattern.signrule;
+
+class SouthAsiaSignRule implements SignRule {
+
+    @Override
+    public String signA2() {
+        return "SouthAsiaA2";
+    }
+
+    @Override
+    public String signA3() {
+        return "SouthAsiaA3";
+    }
+}
