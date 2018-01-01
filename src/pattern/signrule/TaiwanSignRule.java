79a295b8ebb3e49a000d51bd754aaa8d71ee6c4b
fangpapa <qpsa731@gmail.com>
2018-01-01 10:46:23 +0800

    Create TaiwanSignRule.java

---
+++ src/pattern/signrule/TaiwanSignRule.java
@@ -0,0 +1,14 @@
+package pattern.signrule;
+
+class TaiwanSignRule implements SignRule {
+
+    @Override
+    public String signA2() {
+        return "TaiwanA2";
+    }
+
+    @Override
+    public String signA3() {
+        return "TaiwanA3";
+    }
+}
